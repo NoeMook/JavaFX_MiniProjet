fcd03609f13c7f4f1de3d5b3e7d093e0f86c0678
SAKR Ilias <ilias.sakr@gmail.com>
2021-03-25 09:26:15 +0100

    ISSOUX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Bla");
+        primaryStage.setTitle("MARINA");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
