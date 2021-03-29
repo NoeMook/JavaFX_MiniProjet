7cfc3c8a0ffd7de1887bea56d0fcacc3aa0d545a
NoeMook <noe.mouquet@gmail.com>
2021-03-29 09:56:03 +0200

    main & sample.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("@/FXML/sample.fxml"));
         primaryStage.setTitle("MARINA");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
