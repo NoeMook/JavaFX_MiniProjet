84751bb8e0553400f6696656668457aff60bf09b
Marina Plastiras <marina.ekaterini.plastiras@etu.univ-poitiers.fr>
2021-04-30 14:40:44 +0200

    Commentaires option et promotions

--- src/sample/Option.java
+++ src/sample/Option.java
@@ -1,13 +1,14 @@
 package sample;
 
 /**
- * Enulerate type with the options
+ * Enumerate type with the options
  *
  * @author: Group HCI BD 34
  *
  * @version 30/04/2021
  */
 
+//The 3 possible options at Gphy
 public enum Option {
     IMAGERIE, BIOTECHNOLOGIE, PHYSIOLOGIE;
 }
--- src/sample/Promotion.java
+++ src/sample/Promotion.java
@@ -1,13 +1,15 @@
 package sample;
 
 /**
- * Enulerate type with the promotions
+ * Enumerate type with the promotions
  *
  * @author: Group HCI BD 34
  *
  * @version 30/04/2021
  */
 
+
+//The 3 possible class at Gphy
 public enum Promotion {
     L3, M1, M2;
 }
