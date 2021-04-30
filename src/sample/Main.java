4bff6188e67cb5fbacc85e59c0ecce23ba124f25
Marina Plastiras <marina.ekaterini.plastiras@etu.univ-poitiers.fr>
2021-04-30 15:04:46 +0200

    Commentaires list, master et student

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -23,12 +23,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/sample.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/sample.fxml")); //join an fxml file
             Parent root = loader.load();
-            primaryStage.setTitle("Gphy Manager");
+            primaryStage.setTitle("Gphy Manager"); //Name of the window
             primaryStage.setScene(new Scene(root));
             primaryStage.show();
-            listStudent.fillData();
+            listStudent.fillData(); //fill the data for the tests
             Controller controller = loader.getController();
             controller.setMain(this);
         }catch(IOException e){
